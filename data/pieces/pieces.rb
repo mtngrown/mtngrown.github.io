c1362d4c62233370b4e6335f5e125941977625be
Juan Valdez <mtngrown7@yahoo.com>
2014-02-16 12:08:12 -0800

    more clean up on pieces, figured out table display

--- data/pieces/pieces.rb
+++ data/pieces/pieces.rb
@@ -26,24 +26,25 @@ gun mounted on the halftrack is for direct support of the dismounted \
 infantry, not to transform the halftrack into a full fledged fighting vehicle. \
 Remember too that the primary mission of this vehicle is still transport.),
 
-  pals: 'Infantry, particularly engineers and SMGs. Artillery and tanks to knock out what the halftrack is spotting.',
+  pals: %Q(Infantry, particularly engineers and SMGs. Artillery and tanks \
+to knock out what the halftrack is spotting.),
 
-  enemies: "Tanks and infantry. Trucks and regular halftracks may block \
-   but this vehicle can escape encirclement by overrun or fire.",
+  enemies: %Q(Tanks and infantry. Trucks and regular halftracks may block \
+but this vehicle can escape encirclement by overrun or fire.),
 
   fate: "Sacrificed to spot or overburden an enemy unit or lost after a desperate overrun.",
 
   loss: "Shruggable as infantry can always ride on tanks.",
 
   deployment: "In the forefront of the attack. Do not run away from your \
-    passengers. Best to let the regular halftracks do the spotting while this \
-    vehicle hangs back a hex where it can still support by fire. Let the regular \
-    halftracks transport important artillery pieces.",
+passengers. Best to let the regular halftracks do the spotting while this \
+vehicle hangs back a hex where it can still support by fire. Let the regular \
+halftracks transport important artillery pieces.",
 
   historical: "This halftrack, which had a 37mm AT gun mounted on it, was used \
-    by the platoon leaders and company commanders as their command \
-    vehicles. Therefore in a large group of halftracks in an order of battle, every \
-    fourth halftrack unit should be this unit."
+by the platoon leaders and company commanders as their command \
+vehicles. Therefore in a large group of halftracks in an order of battle, every \
+fourth halftrack unit should be this unit."
 }
 
 puts halftrack.to_yaml

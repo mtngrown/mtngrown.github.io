a5c4c2fbc4d10f8f5fac38cf127681a3707bca98
Juan Valdez <mtngrown7@yahoo.com>
2014-09-12 07:25:56 -0700

    started list of games

---
+++ lib/game-list.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+
+GAMELIST = [
+  "PanzerBlitz",
+  "Panzer Leader",
+  "Ogre Designer's Edition",
+  "Tobruk Tank Battles in North Africa",
+  "Arab Israeli Wars",
+  "Ambush!",
+  "Firepower",
+]
